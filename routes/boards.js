4f8c79f01c5321a4213f63469cda15d20a8dd270
Arsh <khokharm@myumanitoba.ca>
2021-06-10 05:17:06 -0500

    Fetch boards owned by authenticated user

--- routes/boards.js
+++ routes/boards.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const verify = require('./verifyToken');
+const pool = require('../dbConfig');
 
-router.get('/', verify, (req, res) => {
-	res.status(200).send(`Got access to ${req.user.email}'s boards`);
+router.get('/', verify, async (req, res) => {
+	auth_user = req.header('auth-user');
+	try {
+		const boards = await pool.query(
+			`SELECT * FROM boards WHERE owner_id='${auth_user}'`
+		);
+		res.send(boards.rows);
+	} catch (error) {
+		console.error(error.message);
+	}
+	//res.status(200).send(`Got access to ${auth_user}'s boards`);
 });
 
 module.exports = router;
