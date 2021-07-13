80d4e4b648e57017c4c9c9f1b37abafbdc9fda1c
Arsh <khokharm@myumanitoba.ca>
2021-07-13 09:45:19 -0500

    Fix ports

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const boardsRoute = require("./routes/boards");
 const listsRoute = require("./routes/lists");
 const tasksRoute = require("./routes/tasks");
 
+app.set("port", process.env.PORT || 5000);
+
 app.use(cors());
 
 app.use(express.json());
@@ -19,4 +21,4 @@ app.get("/api", async (req, res) => {
   res.status(200).send("Welcome to Taskboard API");
 });
 
-app.listen(5000, () => console.log(`Server has started!`));
+app.listen(app.get("port"), () => console.log(`Server has started!`));
